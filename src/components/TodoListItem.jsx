c27d0af813db8abee3dc9f4b3c29d0157f0dab24
Javier Fernandes <javier.fernandes@gmail.com>
2018-04-07 11:53:32 -0300

    Conectando el boton de delete para borrar

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { ListItem, ListItemSecondaryAction, ListItemText } from 'material-ui/List'
 import Checkbox from 'material-ui/Checkbox'
 import IconButton from 'material-ui/IconButton'
 import DeleteIcon from 'material-ui-icons/Delete'
 
-export default function TodoListItem({ item }) {
+import { removeItem } from '../actions/todo'
+import AddTodoListItem from './AddTodoListItem';
+
+function TodoListItem({ item, removeItem }) {
   return (
     <ListItem dense>
       <Checkbox tabIndex={-1} disableRipple />
       <ListItemText primary={item.text} />
       <ListItemSecondaryAction>
         <IconButton aria-label="Delete">
-          <DeleteIcon />
+          <DeleteIcon onClick={() => removeItem(item.id)} />
         </IconButton>
       </ListItemSecondaryAction>
     </ListItem>
   )
 }
+
+export default connect(undefined, 
+  ({
+    removeItem
+  })
+)(TodoListItem)
